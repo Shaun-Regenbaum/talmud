e6ee436ab82da9e3369a135320e67ad841955926
shaunregenbaum <shaunregenbaum@gmail.com>
2022-12-22 22:05:24 -0500

    split texts

--- src/lib/sefaria.ts
+++ src/lib/sefaria.ts
@@ -1,20 +1,19 @@
 // function to fetch the text of a given ref
 import type { OriginalText, SingleText } from './types';
 import { v4 as uuid } from 'uuid';
+import { redis } from './db';
 
-export async function getText(
-	ref: string,
-	index: string
-): Promise<OriginalText> {
+export async function getText(ref: string, num: string): Promise<OriginalText> {
 	ref = ref.replace(' ', '_');
 	ref = ref.replace(',', '%2C');
-	const url = `https://www.sefaria.org/api/texts/${ref}.${index}?context=0`;
+	const url = `https://www.sefaria.org/api/texts/${ref}.${num}?context=0`;
 	const response = await fetch(url);
 	const data = await response.json();
 	const text = data.text;
 	const he = data.he;
 	const heRef = data.heRef;
-    
+	const index = Number(redis.get('originalIndex'));
+
 	const original: OriginalText = {
 		id: uuid(),
 		name: '',
@@ -28,21 +27,26 @@ export async function getText(
 	return original;
 }
 
-function splitTexts(text: string, he: string, heRef: string): SingleText[] {
+function splitTexts(
+	id: string,
+	text: string,
+	he: string,
+	source: string,
+	heRef: string
+): SingleText[] {
 	let texts: SingleText[] = [];
 	let en = text.split(/(?<=\.)\s/);
 	let hebrew = he.split(/(?<=\.)\s/);
-	let heRefSplit = heRef.split(/(?<=\.)\s/);
 
 	for (let i = 0; i < en.length; i++) {
 		texts.push({
 			id: uuid(),
 			name: '',
-			parent: '',
+			parent: id,
 			en: [en[i]],
 			he: [hebrew[i]],
-			source: heRefSplit[i],
-			heSource: heRefSplit[i],
+			source: source,
+			heSource: heRef,
 			index: i,
 		});
 	}
