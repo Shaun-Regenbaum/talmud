599bb8ba960a5455541f0a03e4ce7ca0d2302af9
shaunregenbaum <shaunregenbaum@gmail.com>
2022-12-24 22:20:00 -0500

    search and index

--- src/lib/openai.ts
+++ src/lib/openai.ts
@@ -1,6 +1,9 @@
 import { Configuration, OpenAIApi } from 'openai';
 import { env } from '$env/dynamic/public';
 import { redis } from './db';
+import { SchemaFieldTypes, VectorAlgorithms } from '@redis/search';
+
+import type { RediSearchSchema } from '@redis/search';
 
 const configuration = new Configuration({
 	apiKey: env.PUBLIC_OPENAI_API_KEY,
@@ -20,6 +23,29 @@ export async function storeEmbedding(key: string, embedding: Array<any>) {
 	await redis.json.set(key, '$.embedding', embedding, { NX: true });
 }
 
-export async function searchIndex(embedding: Array<any>): Promise<any> {}
+export async function createSearchIndex() {
+	const indexName = 'searchIndex';
 
-export async function createSearchIndex() {}
+	const schema: RediSearchSchema = {
+		'$.id': {
+			type: SchemaFieldTypes.TEXT,
+			SORTABLE: 'UNF',
+		},
+		'$.embedding': {
+			//@ts-ignore
+			type: SchemaFieldTypes.VECTOR,
+			//@ts-ignore
+			ALGORITHM: VectorAlgorithms.FLAT,
+			//@ts-ignore
+			VECTOR_DIMENSION: 1024,
+		},
+	};
+	await redis.ft.create(indexName, schema, {
+		ON: 'JSON',
+		PREFIX: 'group:',
+	});
+}
+
+export async function searchIndex(embed: any) {
+	await redis.ft.search('idx:embeds', `@embedding:{${embed}}`);
+}
